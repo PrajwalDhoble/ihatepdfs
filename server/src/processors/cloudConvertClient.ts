9cf7960ed42641a67ca88bc7bf8bb939aed73c39
prajwaldhoble5656 <prajwal@worldoffinrep.com>
2026-09-10 22:08:31 +0530

    fix cloudconvert response type

--- server/src/processors/cloudConvertClient.ts
+++ server/src/processors/cloudConvertClient.ts
@@ -71,8 +71,7 @@ async function apiRequest<T>(endpoint: string, init?: RequestInit): Promise<T> {
 
   let json: { data?: T };
   try {
-    json = await res.json();
-  } catch (err) {
+json = (await res.json()) as { data?: T };  } catch (err) {
     throw new AppError(
       `The conversion service returned a response that wasn't valid JSON. (${err instanceof Error ? err.message : "parse error"})`,
       "CLOUDCONVERT_BAD_RESPONSE",
